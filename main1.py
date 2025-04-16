7c763ee10c16b1417e3422174c9f02ef14cce61b
desembers <toastpcyco4786@gmail.com>
2025-04-16 23:22:42 +0900

    백엔드 서버 fastapi

---
+++ main1.py
@@ -0,0 +1,15 @@
+class 가수:
+    def 장르():
+        print("재즈")
+    def 이름():
+        print("아무개")
+    
+가수.이름()
+
+def sayhello():
+    print("안녕하세요")
+    
+sayhello()
+
+for i in range(1,5):
+    print(i)
